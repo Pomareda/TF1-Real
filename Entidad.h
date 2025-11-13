23eb18e8dea97987d2509b4e2f1aaadb21516fb9
Poma <robertfrancisco200@gmail.com>
2025-11-12 22:00:09 -0500

    Se agrego la clase entidad

---
+++ Entidad.h
@@ -0,0 +1,23 @@
+#pragma once
+
+class Entidad
+{
+private:
+
+	int x, y, dx, dy, alto, ancho;
+
+
+public:
+	Entidad();
+	~Entidad();
+
+
+};
+
+Entidad::Entidad()
+{
+}
+
+Entidad::~Entidad()
+{
+}
